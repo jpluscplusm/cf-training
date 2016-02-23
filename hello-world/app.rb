8653a3b038c6a98fee19b1e6b0654844be512139
Jonathan Matthews <contact@jpluscplusm.com>
2016-02-23 11:23:00 +0000

    Stock level API

--- hello-world/app.rb
+++ hello-world/app.rb
@@ -1,12 +1,15 @@
 require "sinatra"
 require "sinatra/json"
 
+STOCK_MAX = ENV.fetch('STOCK_MAX_INT')
+
 class App < Sinatra::Base
-  get '/' do
+  get '/p/:pCode' do |pCode|
     status 200
     content_type "application/json"
 
-    body = { :message => "Hello, world!" }
+    stock_level = Random.rand(STOCK_MAX.to_i)
+    body = { :product => pCode, :stock => stock_level }
 
     json body
   end
